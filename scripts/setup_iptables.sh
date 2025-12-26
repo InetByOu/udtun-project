a41a09ded3e61d78ec3f5b7f6a23cbcde5d3f49e
InetByOu <edisaedi.id@gmail.com>
2025-12-26 07:33:53 +0700

    Delete scripts directory

--- scripts/setup_iptables.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-sysctl -w net.ipv4.ip_forward=1
-iptables -t nat -A PREROUTING -p udp --dport 6000:19999 -j DNAT --to :5667
-iptables -t nat -A POSTROUTING -s 10.66.66.0/24 -j MASQUERADE
